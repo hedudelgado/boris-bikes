640d21809aaa558f81a9958f92318cb49d740aa9
hedudelgado <hedu.delgado@gmail.com>
2016-01-20 16:51:59 +0000

    class and test working with limited bikes

--- lib/Docking_station.rb
+++ lib/Docking_station.rb
@@ -4,17 +4,27 @@ class DockingStation
    
    attr_reader :bike
 
+   def initialize
+   	@bike = []
+   end
+
    def release_bike
-   	@bike
+   		if @bike.size == 0
+   			raise 'No bikes'
+   		else
+   			@bike.first
+   		end
    end
 
    def dock(bike)
-   	@bike = bike
+   		if @bike.size > 20
+   			raise 'Station Full'
+   		else
+   			@bike << bike
+   		end
    end
 
 
 end
-
-
 
 
--- spec/Docking_station_spec.rb
+++ spec/Docking_station_spec.rb
@@ -11,8 +11,24 @@ describe DockingStation do
         subject.dock(bike)
         expect(subject.release_bike).to eq bike
         end
+        it 'raises an error when there are no bikes available' do 
+            expect {subject.release_bike}.to raise_error 'No bikes' 
+        end
     end
 end
+
+#---------------
+
+
+describe DockingStation do  
+    describe '#dock' do 
+        it 'raises an error when the station is full' do
+            21.times { subject.dock(Bike.new) }
+            expect {subject.dock Bike.new  }.to raise_error 'Station Full'
+        end
+    end
+end
+#---------------
 
 describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument }
@@ -22,6 +38,6 @@ describe DockingStation do
     it 'returns docked bikes' do
         bike = Bike.new
         subject.dock(bike)
-        expect(subject.bike).to equal bike
+        expect(subject.bike.last).to equal bike
     end
 end

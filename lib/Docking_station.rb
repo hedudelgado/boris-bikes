e356d8569e3a038ba76a9583b8f62383ac1d3155
hedudelgado <hedu.delgado@gmail.com>
2016-01-20 12:33:12 +0000

    chapter 12 building the nobikes Error!

--- lib/Docking_station.rb
+++ lib/Docking_station.rb
@@ -1,5 +1,20 @@
+require_relative 'Bike'
 
 class DockingStation
+   
+   attr_reader :bike
+
+   def release_bike
+   	@bike
+   end
+
+   def dock(bike)
+   	@bike = bike
+   end
 
 
 end
+
+
+
+
--- spec/Docking_station_spec.rb
+++ spec/Docking_station_spec.rb
@@ -1,5 +1,27 @@
 require 'Docking_station'
 
 describe DockingStation do
+    it {is_expected.to respond_to :release_bike}
+end
 
+describe DockingStation do
+  describe '#release_bike' do 
+    it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+        end
+    end
+end
+
+describe DockingStation do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+end
+
+describe DockingStation do
+    it 'returns docked bikes' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.bike).to equal bike
+    end
 end
